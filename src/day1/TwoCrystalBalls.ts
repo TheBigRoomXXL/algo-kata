2de984175a12b4485a01bf611f1e52a3a159336a
TheBigRoomXXL <seb.lovergne@gmail.com>
2023-09-01 11:05:18 +0200

    feat: Two Crytal balls

---
+++ src/day1/TwoCrystalBalls.ts
@@ -0,0 +1,13 @@
+export default function two_crystal_balls(breaks: boolean[]): number {
+    let increment = Math.floor(Math.sqrt(breaks.length))
+    let i = 0
+    while (i < breaks.length) {
+        i += increment
+        if (breaks[i]) {
+            for (let j = i - increment; j <= i; j++) {
+                if (breaks[j]) { return j }
+            }
+        }
+    }
+    return -1
+}
